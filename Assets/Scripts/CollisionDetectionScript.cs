03f76f4fa541973552334db125b732502d20f782
Jess Hek <108735805+what-the-hek@users.noreply.github.com>
2025-06-29 16:41:35 +0800

    prefab objects with different tags give different score points

--- Assets/Scripts/CollisionDetectionScript.cs
+++ Assets/Scripts/CollisionDetectionScript.cs
@@ -7,9 +7,24 @@ public class CollisionDetectionScript : MonoBehaviour
     // player collides with a trigger collider
     private void OnTriggerEnter2D(Collider2D other)
     {
-        globalVariables.totalScore += 1;
-        Debug.Log("hit trigger: " + other.name);
-        Debug.Log("total score: " + globalVariables.totalScore);
+        if (other.CompareTag("commonNPC"))
+        {
+            globalVariables.totalScore += 1;
+            Debug.Log("hit common trigger: " + other.name);
+            Debug.Log("total score: " + globalVariables.totalScore);
+        }
+        else if (other.CompareTag("uncommonNPC"))
+        {
+            globalVariables.totalScore += 2;
+            Debug.Log("hit uncommon trigger: " + other.name);
+            Debug.Log("total score: " + globalVariables.totalScore);
+        }
+        else
+        {
+            Debug.Log("hit untagged object");
+            Debug.Log("total score: " + globalVariables.totalScore);
+        }
+
     }
 
     // player collides with a non-trigger collider
